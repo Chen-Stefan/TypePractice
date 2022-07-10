5c8463bcb2387632c2d39dd18af6a669ca955b21
Stefan Chen <zchen158@my.bcit.ca>
2022-07-09 22:27:56 -0700

    declare typescript arrays

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-let sales: number = 123_456_789
-let course: string = 'TypeScript'
+let numbers: number[] = []
+numbers.forEach(n => n.toString)

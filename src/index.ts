6c55b4060ff569bb8abeb74340e7ca1ec65062b8
Stefan Chen <zchen158@my.bcit.ca>
2022-07-10 01:46:24 -0700

    Deal with null and undefined

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-// Literal type with union
+function greet(name: string | null | undefined) {
+  if (name)
+    console.log(name.toUpperCase())
+  else
+    console.log('Hola')
+} 
 
-type Quantity = 50 | 100
-let quantity: Quantity = 100
-
-type Metric = 'cm' | 'inch'
+greet(undefined)

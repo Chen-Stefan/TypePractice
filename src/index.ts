07fcbb8a95bb399d0c8000633e6c4d0ce3d2f0d6
Stefan Chen <zchen158@my.bcit.ca>
2022-07-09 22:31:06 -0700

    declare typescript tuples

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-let numbers: number[] = []
-numbers.forEach(n => n.toString)
+//Tuples are useful only in pair of 2
+let user: [number, string] = [1, 'Stefan']
+user[0].toString
+user[1].charAt

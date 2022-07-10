0ed5658c762f4d25f2ec2284d9224e3c79f264e7
Stefan Chen <zchen158@my.bcit.ca>
2022-07-10 01:53:43 -0700

    Use optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-function greet(name: string | null | undefined) {
-  if (name)
-    console.log(name.toUpperCase())
-  else
-    console.log('Hola')
-} 
+type Customer = {
+  birthday?: Date
+}
 
-greet(undefined)
+function getCustomer(id: number): Customer | null | undefined{
+  return id === 0 ? null : {birthday: new Date()}
+}
+
+let customer = getCustomer(0)
+
+console.log(customer?.birthday?.getFullYear())
+
+//Optional element access operator
+customers?.[0]
+
+//Optional call, will get executed only if log is referencing an actual function. Ohterwise, it will get undefined
+let log: any = null
+log?.('a')

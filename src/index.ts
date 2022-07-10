43f1ec2220fe39309040c0de5ca820493b28b8ef
Stefan Chen <zchen158@my.bcit.ca>
2022-07-10 01:01:39 -0700

    declare objects

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-function calculateTax(income: number, taxYear = 2022): number {
-  if(taxYear < 2022)
-    return income * 1.2
-  return income * 1.3
-}
+// read only so you can't modify
+let employee: {
+  readonly id: number,
+  name: string
+  retire: (date: Date) => void
+} = {id: 1, name: '', retire: (date: Date) => {
+  console.log(date)
+}}
 
-calculateTax(10_000)
-
-// void suggests no return value
-function whatever(): void {
-  console.log('haha')
-}
+employee.id = 0

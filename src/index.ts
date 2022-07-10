329a99e869221a76a28f3cd36cb25d7a2969e6cb
Stefan Chen <zchen158@my.bcit.ca>
2022-07-09 22:38:10 -0700

    Declare typescript enums

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-//Tuples are useful only in pair of 2
-let user: [number, string] = [1, 'Stefan']
-user[0].toString
-user[1].charAt
+//Use const keyword with enum, compiler will generate much optimized code 
+
+const enum Size {Small = 1, Medium, Large}
+// enum Size {Small = 's', Medium = 'm', Large = 'l'}
+let mySize: Size = Size.Medium

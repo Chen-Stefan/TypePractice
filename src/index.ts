e91019560948f7f7d7266684354de07cd00f85fe
Stefan Chen <zchen158@my.bcit.ca>
2022-07-09 22:01:48 -0700

    learn basic types and TypeScript specific type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-let age: number = 20
+// Built-in types: number, string, boolean, null, undefined, object
+// Typescript specific: any, unknown, never, enum, tuple
+
+let sales: number = 123_456_789
+let course: string = 'TypeScript'

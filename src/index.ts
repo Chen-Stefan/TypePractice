79f15191455925124054369fb503a8a266757dde
Stefan Chen <zchen158@my.bcit.ca>
2022-07-11 00:08:13 -0700

    Declare array of tuples

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-let x: any = 'Hello'
+let employee: [number, string][]
 
-x = 30
+employee = [
+  [1, 'stefan'],
+  [2, 'brad'],
+  [3, 'damon']
+]

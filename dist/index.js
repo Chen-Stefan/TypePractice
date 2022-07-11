799ed229dff7670805d022d1efab0f137c94516b
Stefan Chen <zchen158@my.bcit.ca>
2022-07-11 16:29:58 -0700

    Declare enums with default value

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-let num = 3;
-console.log(num);
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-let employee: [number, string][]
+// Enum
 
-employee = [
-  [1, 'stefan'],
-  [2, 'brad'],
-  [3, 'damon']
-]
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right'
+}

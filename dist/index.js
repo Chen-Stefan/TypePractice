e1bd6ba98d996298c7d189032746c7ec674faa5a
Stefan Chen <zchen158@my.bcit.ca>
2022-07-11 00:04:32 -0700

    Use any type

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,3 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === 'number') {
-        return weight * 2.2;
-    }
-    else {
-        return parseInt(weight) * 2.2;
-    }
-}
-kgToLbs(10);
-kgToLbs('10kg');
+let num = 3;
+console.log(num);
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
 // Built-in types: number, string, boolean, null, undefined, object
 // Typescript specific: any, unknown, never, enum, tuple
 
-type Customer = {
-  birthday?: Date
-}
+let x: any = 'Hello'
 
-function getCustomer(id: number): Customer | null | undefined{
-  return id === 0 ? null : {birthday: new Date()}
-}
-
-let customer = getCustomer(0)
-
-console.log(customer?.birthday?.getFullYear())
-
-//Optional element access operator
-customers?.[0]
-
-//Optional call, will get executed only if log is referencing an actual function. Ohterwise, it will get undefined
-let log: any = null
-log?.('a')
+x = 30
